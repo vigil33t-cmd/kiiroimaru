05386b94725f33d2e8f0732c38c13b8a0663c7ae
bluepanee <53943505+bluepanee@users.noreply.github.com>
2022-11-01 23:06:44 +0500

    Множество изменений.
    Изменена структура бд. Теперь борды хранятся в коллекции boards, а ссылки на треды находятся в boards["threads"]
    Хедер теперь подгружается название и описание доски.
    Добавлен более менее адекватный обработчик чоч
    Теперь скрытые треды/посты не отрисовываются на странице.(Добаротать)
    И еще несколько мелких штук.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort
 import pymongo
 from werkzeug.utils import secure_filename
 from bson import DBRef
 
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 app = Flask(__name__)
 app.secret_key = "133713371337"
 app.config['UPLOAD_FOLDER'] = 'uploads'
+
+app.register_error_handler(404, page_not_found)
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 CONNECTION_STRING = "mongodb://90.151.59.128:27017"
@@ -18,32 +23,39 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
-def renderIndex():
-    return render_template("index.html")
+def root():
+    return render_template("index.html", boards=db.boards.find({}))
 
 
 @app.route("/<board>/")
-def renderBoard(board):
-    try:
-        db.validate_collection(board)  # Try to validate a collection
-    except pymongo.errors.OperationFailure:
-        return render_template("404.html")
-    threads = db.get_collection(board).find({"is_thread": True})
-    return render_template("board.html", threads=threads, db=db, isThread=False)
+def board(board):
+    b = db.boards.find_one({"board_name": board})
+    
+    if not b:
+        abort(404)
+
+    threads = []
+    for t in b["threads"]:
+        if db.dereference(t)["hidden"]:
+            continue
+        threads.append(db.dereference(t))
+    return render_template("board.html", threads=threads, db=db, isThread=False, board=b    )
 
 
 @app.route("/<board>/<int:thread_id>")
-def renderThread(board, thread_id):
-    try:
-        db.validate_collection(board)  # Try to validate a collection
-    except pymongo.errors.OperationFailure:
-        return render_template("404.html")
-    # print(db.get_collection(board).find({'id':thread_id}))
-    if list(db.get_collection(board).find({'id':thread_id})) == [] or db.get_collection(board).find_one({'id':thread_id})['is_thread'] == False or db.posts.find_one({'id':thread_id})['hidden'] == True:
-        print('a')
-        return render_template('404.html')
-    thread = db.get_collection(board).find_one({"is_thread": True, "id": thread_id})
-    return render_template("thread.html", thread=thread, db=db, isThread=True)
+def thread(board, thread_id):
+    thread = db.posts.find_one({"id": thread_id})
+    b = db.boards.find_one({"board_name": board})
+
+    # фиксми блять
+    if not thread or \
+       not b or \
+       not thread["is_thread"] or \
+       thread["hidden"] or \
+       DBRef("posts", thread["_id"], "yobach") not in b["threads"]:
+        abort(404)
+
+    return render_template("thread.html", thread=thread, db=db, isThread=True, board=b)
 
 
 @app.post('/api/upload')
@@ -54,13 +66,12 @@ def upload_file():
         f.save(secure_filename(f.filename))            
         return ""
     return "invalid filetype"
-    
 
-@app.post("/api/thread.create") # Кто функции апи через точку пишет блять?
-def makeThread():
+@app.post("/api/thread.create")
+def thread_create():
     data = request.values
 
-    board = data.get("board")
+    board_id = int(data.get("board_id"))
     title = data.get("title")
     text = data.get("text")
 
@@ -78,16 +89,15 @@ def makeThread():
     }).inserted_id
 
     ref = DBRef("posts", inserted_post_id, "yobach")
-    db.get_collection(board).insert_one({"ref": ref, "is_thread": True, "id": post_id})
+    db.boards.update_one({"board_id": board_id}, {"$push": {"threads": ref}})
 
     return ""
 
 @app.post("/api/thread.answer")
-def answerThread():
+def thread_answer():
     data = request.values
 
-    board = data.get("board")
-    thread = int(data.get("thread"))
+    thread_id = int(data.get("thread_id"))
     text = data.get("text")
 
     post_id = db.posts.count_documents({}) + 1
@@ -96,27 +106,49 @@ def answerThread():
         "id": post_id,
         "timestamp": "13/03/37 13:37",
         "is_thread": False,
-        "thread": thread,
+        "thread_id": thread_id, # Вместо айди поместить сюда DBRef ссылку на тред?
         "text": text,
-        "hidden":False
-        
+        "hidden": False
     }).inserted_id
 
     ref = DBRef("posts", inserted_post_id, "yobach")
-    db.get_collection(board).insert_one({"ref": ref, "is_thread": False, "id": post_id})
-    
-    db.posts.update_one({"id": thread}, {'$push': {"posts": ref}})
+    db.posts.update_one({"id": thread_id}, {'$push': {"posts": ref}})
 
     return ""
-    
-@app.post("/api/hide")
-def hide():
+
+@app.post("/api/board.create")
+def board_create():
     data = request.values
-    board = data.get("board")
-    id = int(data.get("id"))
-    
-    db.get_collection(board).update_one({"id":id}, {"$set": {"hidden": True}})
-    
+
+    board_id = db.boards.count_documents({}) + 1
+    board_name = data.get("name")
+    board_desc = data.get("desc")
+
+    db.boards.insert_one({
+        "board_id": board_id,
+        "board_name": board_name,
+        "board_desc": board_desc,
+        "threads": []
+    })
+
+    return ""
+
+@app.post("/api/post.hide")
+def post_hide():
+    data = request.values
+    post_id = int(data.get("post_id"))
+
+    db.posts.update_one({"id": post_id}, {"$set": {"hidden": True}})
+
+    return ""
+
+@app.post("/api/post.unhide") # post.show?
+def post_unhide():
+    data = request.values
+    post_id = int(data.get("post_id"))
+
+    db.posts.update_one({"id": post_id}, {"$set": {"hidden": False}})
+
     return ""
 
 app.run(debug=True)
--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,33 @@ import requests as r
 
 
 # data = {
-#     "board": "a",
-#     "title": "Интересно, а какой предел названия треда? ААаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа",
-#     "text": "Одинокий тредик с очень длинным заголовком."
+#     "board_id": 2,
+#     "title": "Еще один тестовый тред в тестовой борде bread",
+#     "text": "лорем ипсум"
 # }
 
 # response = r.post("http://localhost:5000/api/thread.create", data=data)
 # print(response)
 
-data = {
-    "thread": 1,
-    "board": "a",
-    "text": "Бамп."
-}
+# data = {
+#     "thread_id": 18,
+#     "text": "пук"
+# }
 
-response = r.post("http://localhost:5000/api/thread.answer", data=data)
-print(response)
+# response = r.post("http://localhost:5000/api/thread.answer", data=data)
+# print(response)
 
+# data = {
+#     "name": "bread",
+#     "desc": "fresh bread every day",
+# }
 
+# response = r.post("http://localhost:5000/api/board.create", data=data)
+# print(response)
+
+# data = {
+#     "post_id": 48,
+# }
+
+# response = r.post("http://localhost:5000/api/post.hide", data=data)
+# print(response)
